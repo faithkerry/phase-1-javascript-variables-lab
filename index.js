707720896618a42fe4665b469d4bf1ca2caee439
faith-kerry <kerubofaith944@gmail.com>
2025-09-16 10:59:22 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution in this file!
+const companyName = 'Scuber';
+console.log(companyName);
+
+let mostProfitableNeighborhood = 'Chelsea';
+console.log(mostProfitableNeighborhood);
+
+let companyCeo = "Susan Smith";
+console.log(companyCeo);
+
+module.exports = {
+    companyName,
+    mostProfitableNeighborhood,
+    companyCeo,
+};
+
+
+
